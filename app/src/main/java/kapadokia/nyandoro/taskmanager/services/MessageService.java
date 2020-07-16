d7964d21914d70caedd96b834f2adbd5ef97411a
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-17 02:06:58 +0300

    added text input field

---
+++ app/src/main/java/kapadokia/nyandoro/taskmanager/services/MessageService.java
@@ -0,0 +1,11 @@
+package kapadokia.nyandoro.taskmanager.services;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+// simple interface that will implement the news texts in our landing page
+public interface MessageService {
+
+    @GET("messages") // tells the retrofit that when we call this method we should match it to a get request of the base Url
+    Call<String> getMessage();
+}
---
+++ app/src/main/java/kapadokia/nyandoro/taskmanager/services/ServiceBuilder.java
@@ -0,0 +1,20 @@
+package kapadokia.nyandoro.taskmanager.services;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceBuilder {
+
+    // building the URL and automatically handle the data mapping
+    private static final String URL = "URL";  // replace with a working url
+
+    private static Retrofit.Builder builder = new Retrofit.Builder().baseUrl(URL)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = builder.build();
+
+    // helper method that enables us to build services
+    public static <S> S buildService(Class<S> serviceType){
+        return retrofit.create(serviceType);
+    }
+}
