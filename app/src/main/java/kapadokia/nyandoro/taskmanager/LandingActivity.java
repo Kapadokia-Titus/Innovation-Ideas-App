da6fd29fe82e4f20f1c387d1edfc395295e6e5e0
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-07-20 03:30:28 +0300

    still struggling

--- app/src/main/java/kapadokia/nyandoro/taskmanager/LandingActivity.java
+++ app/src/main/java/kapadokia/nyandoro/taskmanager/LandingActivity.java
@@ -20,24 +20,21 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
 
-        // this will act as implementation of our interface that maps our restful endpoints
         MessageService taskService = ServiceBuilder.buildService(MessageService.class);
-        Call<String> call = taskService.getMessage();
+        Call<String> call = taskService.getMessages();
 
-        // we are then calling enqueue which behind the scenes will add it to an asynchronous
-        // request queue that retrofit handles for us
         call.enqueue(new Callback<String>() {
             @Override
-            public void onResponse(Call<String> call, Response<String> response) {
+            public void onResponse(Call<String> request, Response<String> response) {
                 ((TextView)findViewById(R.id.message)).setText(response.body());
             }
 
             @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                ((TextView)findViewById(R.id.message)).setText(R.string.request_failed);
+            public void onFailure(Call<String> request, Throwable t) {
+                ((TextView)findViewById(R.id.message)).setText("Request Failed");
             }
         });
-      }
+    }
 
     public void GetStarted(View view){
         Intent intent = new Intent(this, IdeaListActivity.class);
--- app/src/main/java/kapadokia/nyandoro/taskmanager/services/MessageService.java
+++ app/src/main/java/kapadokia/nyandoro/taskmanager/services/MessageService.java
@@ -6,6 +6,7 @@ import retrofit2.http.GET;
 // simple interface that will implement the news texts in our landing page
 public interface MessageService {
 
-    @GET("messages") // tells the retrofit that when we call this method we should match it to a get request of the base Url
-    Call<String> getMessage();
+    @GET("/messages") // tells the retrofit that when we call this method we should match it to a get request of the base Url
+    Call<String> getMessages();
+
 }
